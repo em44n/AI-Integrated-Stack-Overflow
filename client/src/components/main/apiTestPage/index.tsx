73af444d7d66fea512b5dad34241e496aa17d6cb
Rida Jawed <rida.jawed@gmail.com>
2024-11-01 12:52:37 -0400

    Add HuggingFace sentence similarity api call, service, and temporary testing component.

---
+++ client/src/components/main/apiTestPage/index.tsx
@@ -0,0 +1,11 @@
+import SimilarityChecker from './similarityChecker';
+
+const APITestPage = () => (
+  <>
+    <div className='space_between right_padding'>
+      <SimilarityChecker /> {/* Include the TextQuery component here */}
+    </div>
+  </>
+);
+
+export default APITestPage;
---
+++ client/src/services/sentenceSimilarityService.ts
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+import getSimilarity from './huggingFace/sentenceSimilarityAPI';
+
+// Function to filter queries based on similarity to the main sentence
+// NOTE: The queries input type may be changed depending on the usage of this function
+async function filterQueriesBySimilarity(
+  mainSentence: string,
+  queries: { _id: string; text: string }[],
+): Promise<string[]> {
+  const matchingIds: string[] = [];
+
+  for (const query of queries) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const similarity = await getSimilarity(mainSentence, query.text);
+      console.log('Similarity:', similarity);
+      if (similarity > 0.5) {
+        matchingIds.push(query._id);
+      }
+      console.log('Matching IDs:', matchingIds);
+    } catch (error) {
+      console.error(`Error comparing with query ID ${query._id}:`, error);
+    }
+  }
+
+  return matchingIds;
+}
+
+export default filterQueriesBySimilarity;
