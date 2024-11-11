afbc797bbbb835c0cab1ad0aae4c64586d994c51
Veena K <vkodakirthi@gmail.com>
2024-11-11 14:04:48 +0900

    implement top 3 questions for similar questions while user is writing question

--- client/src/services/sentenceSimilarityService.ts
+++ client/src/services/sentenceSimilarityService.ts
@@ -27,29 +27,33 @@ async function filterQueriesBySimilarity(
   return matchingIds;
 }
 
-// Function to filter queries based on similarity to the main sentence
-// NOTE: The queries input type may be changed depending on the usage of this function
+/**
+ * Function to get the similarity of a sentence compared to Questions
+ * @param mainSentence the sentence (string) to comapre to other Questions
+ * @param questions array of Questions to compare main sentence with
+ * @returns array of top 3 questions matching the mainSentence, can return anywhere from 0-3 Questions
+ */
 async function filterQuestionsBySimilarity(
   mainSentence: string,
-  queries: Question[],
+  questions: Question[],
 ): Promise<Question[]> {
-  const matchingIds: Question[] = [];
-
-  for (const query of queries) {
+  const scoredQuestions: { question: Question; similarity: number }[] = [];
+  for (const question of questions) {
     try {
       // eslint-disable-next-line no-await-in-loop
-      const similarity = await getSimilarity(mainSentence, query.text);
+      const similarity = await getSimilarity(mainSentence, question.text);
       console.log('Similarity:', similarity);
-      if (similarity > 0.5) {
-        matchingIds.push(query);
+      if (similarity > 0.3) {
+        scoredQuestions.push({ question, similarity });
       }
-      console.log('Matching IDs:', matchingIds);
     } catch (error) {
-      console.error(`Error comparing with query ID ${query._id}:`, error);
+      console.error(`Error comparing with query ID ${question._id}:`, error);
     }
   }
-
-  return matchingIds;
+  return scoredQuestions
+    .sort((a, b) => b.similarity - a.similarity)
+    .slice(0, 3)
+    .map(scoredQuestion => scoredQuestion.question);
 }
 
 export { filterQueriesBySimilarity, filterQuestionsBySimilarity };
